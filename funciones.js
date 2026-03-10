d2b522bd42f122878c590b9cef44285e30b56498
mlagos <marlonlagos036@gmail.com>
2026-03-10 14:04:16 -0600

    video 9 curso gas avanzados

--- funciones.js
+++ funciones.js
@@ -10,3 +10,9 @@ function obtenerDatosHTML (nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre)
     .getContent();
 }
+
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('1UGfa7-km9g3EsuBYufYayjSXSVhe-5XlYAUkZb1seh8').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
